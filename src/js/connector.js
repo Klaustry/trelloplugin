9bedda179abeb852de808dcd0e757377153514ad
Oliver65535 <oo-tarasov@ya.ru>
2022-03-24 23:31:45 +0300

    change text

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,7 +4,7 @@ TrelloPowerUp.initialize({
   'card-badges': function (t, options) {
     return t.card('all').then(function (card) {
       console.log(card)
-      return [{ text: `💳 ${card.idShort}`, text: 'USDT' }]
+      return [{ text: `💳 ${card.idShort} USDT` }]
     })
   },
 })
