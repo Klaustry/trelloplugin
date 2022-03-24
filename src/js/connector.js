2d977d4875e2d9b38feefdd8cd30fcdfefc0a3ce
Oliver65535 <oo-tarasov@ya.ru>
2022-03-25 00:08:24 +0300

    retun in one function

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,31 +1,29 @@
 console.log('hello world!')
 
+const getCardRewardInfo = function (card) {
+  if (card.idShort > 2 && card.idShort < 5)
+    return [
+      { title: 'Pay', text: `💳 ${card.idShort} USDT` },
+      {
+        title: 'Reward',
+        icon:
+          'https://raw.githubusercontent.com/broxus/ton-assets/master/icons/USDTv3/logo.svg',
+        text: 'USDT',
+      },
+    ]
+  else return []
+}
+
 TrelloPowerUp.initialize({
   'card-badges': function (t, options) {
     return t.card('all').then(function (card) {
       console.log(card)
-      return [
-        { text: `💳 ${card.idShort} USDT` },
-        {
-          icon:
-            'https://raw.githubusercontent.com/broxus/ton-assets/master/icons/USDTv3/logo.svg',
-          text: 'USDT',
-        },
-      ]
+      return getCardRewardInfo(card)
     })
   },
   'card-detail-badges': function (t, options) {
     return t.card('all').then(function (card) {
-      console.log(card)
-      return [
-        { title: 'Pay', text: `💳 ${card.idShort} USDT` },
-        {
-          title: 'Reward',
-          icon:
-            'https://raw.githubusercontent.com/broxus/ton-assets/master/icons/USDTv3/logo.svg',
-          text: 'USDT',
-        },
-      ]
+      return getCardRewardInfo(card)
     })
   },
 })
