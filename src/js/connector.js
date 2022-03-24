a437288bf238d0f6b551b66716bc1c8321b9d8d9
Oliver65535 <oo-tarasov@ya.ru>
2022-03-24 23:52:51 +0300

    add detail badges

--- src/js/connector.js
+++ src/js/connector.js
@@ -14,4 +14,18 @@ TrelloPowerUp.initialize({
       ]
     })
   },
+  'card-detail-badges': function (t, options) {
+    return t.card('all').then(function (card) {
+      console.log(card)
+      return [
+        { title: 'Pay', text: `💳 ${card.idShort} USDT` },
+        {
+          title: 'Reward',
+          icon:
+            'https://raw.githubusercontent.com/broxus/ton-assets/master/icons/USDTv3/logo.svg',
+          text: 'USDT',
+        },
+      ]
+    })
+  },
 })
