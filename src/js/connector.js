509ffb24ed23cad53374a43cd21ec44903d5db1f
Oliver65535 <oo-tarasov@ya.ru>
2022-03-25 17:24:17 +0300

    change icon

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,9 +1,7 @@
 console.log('hello world!')
 
-var WHITE_ICON =
-  'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg'
-var BLACK_ICON =
-  'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg'
+var WHITE_ICON = 'https://img.icons8.com/ios/344/decentralized-network.png'
+var BLACK_ICON = 'https://img.icons8.com/ios/344/decentralized-network.png'
 
 var onBtnClick = function (t, opts) {
   console.log('Someone clicked the button')
