175dc6e1e8a2a3169a16d6c720a188c5890f171b
Oliver65535 <oo-tarasov@ya.ru>
2022-03-25 17:17:19 +0300

    add board button

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,14 +1,23 @@
 console.log('hello world!')
 
+var WHITE_ICON =
+  'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg'
+var BLACK_ICON =
+  'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg'
+
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button')
+}
+
 const getCardRewardInfo = function (card) {
-  if (card.idShort > 2 && card.idShort < 5)
+  if (card.idShort > 2 && card.idShort < 6)
     return [
-      { title: 'Pay', text: `💳 ${card.idShort} USDT` },
+      { title: 'Reward', text: `💳 ${card.idShort * 100} USDT` },
       {
-        title: 'Reward',
+        title: 'Status',
         icon:
           'https://raw.githubusercontent.com/broxus/ton-assets/master/icons/USDTv3/logo.svg',
-        text: 'USDT',
+        text: 'Active',
       },
     ]
   else return []
@@ -25,5 +34,31 @@ TrelloPowerUp.initialize({
     return t.card('all').then(function (card) {
       return getCardRewardInfo(card)
     })
+  },
+  'board-buttons': function (t, opts) {
+    return [
+      {
+        // we can either provide a button that has a callback function
+        icon: {
+          dark: WHITE_ICON,
+          light: BLACK_ICON,
+        },
+        text: 'Callback',
+        callback: onBtnClick,
+        condition: 'edit',
+      },
+      {
+        // or we can also have a button that is just a simple url
+        // clicking it will open a new tab at the provided url
+        icon: {
+          dark: WHITE_ICON,
+          light: BLACK_ICON,
+        },
+        text: 'URL',
+        condition: 'always',
+        url: 'https://trello.com/inspiration',
+        target: 'Inspiring Boards', // optional target for above url
+      },
+    ]
   },
 })
