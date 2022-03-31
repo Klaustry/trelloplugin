b7b9d0b2879316c35cf0dc4f7453b199021105c4
Oliver65535 <oo-tarasov@ya.ru>
2022-03-31 17:37:01 +0300

    main

--- src/js/contract.js
+++ src/js/contract.js
@@ -28,24 +28,6 @@ document.getElementById('getCard') &&
       getCard()
     })
 
-var t = window.TrelloPowerUp.iframe()
-const context = t.getContext()
-//console.log(t.getContext())
-
-document.getElementById('addRewardButton') &&
-  document
-    .getElementById('addRewardButton')
-    .addEventListener('click', function (event) {
-      sendRewardParams(
-        document.getElementById('idSelectBlockChain').value,
-        document.getElementById('idAmount').value,
-        document.getElementById('idSelectToken').value,
-      )
-      addCard(context.card, context.member, parseInt(amount), token)
-      console.log('addReward Clicked!')
-      t.closePopup()
-    })
-
 const contractAddress = '0x2C1b05D739aeCd2006c1a685BE3cAeeD1522895D'
 const abi = [
   {
