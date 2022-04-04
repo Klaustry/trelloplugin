8bf2e1cb698659fcfc54f3053e5c795e1f172775
Oliver65535 <oo-tarasov@ya.ru>
2022-04-04 21:11:34 +0300

    m

--- src/js/connector.js
+++ src/js/connector.js
@@ -8,13 +8,13 @@ var EVER = 'https://s2.coinmarketcap.com/static/img/coins/64x64/7505.png'
 const getCardRewardInfo = function (t, cardID) {
   return getCard(cardID.id)
     .then(function (e) {
-      t.remove('card', 'shared', ['performer', 'exist', 'mykey', 'amount'])
-      // if (e.exists) {
-      //   t.set('card', 'shared', 'exist', e.exists)
-      //   t.set('card', 'shared', 'performer', e.performerID)
-      //   t.set('card', 'shared', 'amount', e.amount)
-      //   t.set('card', 'shared', 'token', e.token)
-      // }
+      //t.remove('card', 'shared', ['performer', 'exist', 'mykey', 'amount'])
+      if (e.exists) {
+        t.set('card', 'shared', 'exist', e.exists)
+        t.set('card', 'shared', 'performer', e.performerID)
+        t.set('card', 'shared', 'amount', e.amount)
+        t.set('card', 'shared', 'token', e.token)
+      }
 
       t.getAll().then((e) => console.log(e))
       //console.log('get card info', e)
