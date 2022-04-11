79336a4b0c60279fb39c4593a76820401922836e
Oliver65535 <oo-tarasov@ya.ru>
2022-04-11 19:53:29 +0300

    m

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const tokens = [
+  {
+    symbol: 'USDT',
+    decimals: 18,
+    contractAddress: '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd',
+  },
+  {
+    symbol: 'DAI',
+    decimals: 18,
+    contractAddress: '0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867',
+  },
+  {
+    symbol: 'USDC',
+    decimals: 18,
+    contractAddress: '0x813b24C0f3BafEB03DB596B97D487ac362756961',
+  },
+]
+
+export const trelloContractAddress =
+  '0x2CB85bE6f340B231952C39c079C77A59e8B86f7f'
