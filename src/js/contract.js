60243d8fed28dcfa4e207fdc295736d9ca252d20
Oliver65535 <oo-tarasov@ya.ru>
2022-04-12 13:24:23 +0300

    m

--- src/js/contract.js
+++ src/js/contract.js
@@ -1,4 +1,4 @@
-//import { ethers } from 'ethers'
+import { ethers } from 'ethers'
 import trelloAbi from '../js/abi/trelloContract.abi.json'
 import rootTokenAbi from '../js/abi/rootToken.abi.json'
 import { trelloContractAddress, tokens } from '../constants'
